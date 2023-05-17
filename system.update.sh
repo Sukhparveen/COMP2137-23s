f5b258e5de67c8ed50d1109284591c698ad80d33
sukhparveen <200498865@student.georgianc.on.ca>
2023-05-17 14:16:16 -0400

    created syatem update

---
+++ system.update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#created may 17 by sukhparveen to perform update task for the lab
+
+#update the software cache in case it is needed
+sudo apt update
+#upgrade using new software package versions
+sudo apt upgrade 
+
